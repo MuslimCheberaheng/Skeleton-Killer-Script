48c3460fd512bd773bf4d9075b67e2d206c7169e
Muslim Cheberaheng <66585845+MuslimCheberaheng@users.noreply.github.com>
2020-06-20 16:41:44 +0700

    Update HealthBarScriptMonster.cs

--- HealthBarScriptMonster.cs
+++ HealthBarScriptMonster.cs
@@ -5,50 +5,19 @@ using UnityEngine.UI;
 
 public class HealthBarScriptMonster : MonoBehaviour
 {
-
     Image healthBar;
     float maxHealthMonster = 100;
-    public static float healthMonster;
-    //public Skeleton ActiveDead;
+    public static float healthMonster;    
     public GameObject Skeleton;
-
-
 
     void Start()
     {
         healthBar = GetComponent<Image>();
-        healthMonster = maxHealthMonster;
-        //ActiveDead = GetComponent<Skeleton>();
-
-
+        healthMonster = maxHealthMonster;        
     }
-
-
-
+    
     void Update()
     {
         healthBar.fillAmount = healthMonster / maxHealthMonster;
-    }
-
-
-   // void Die()
-   // {
-    //    if (healthMonster == 0)
-    //    {
-    //        Die();
-    //        print("225");
-     //   }
-
-}
-
-
-   // void ReduceHealth()
-    //{
-    //    if (healthMonster == 0f)
-     //   {
-     //       Skeleton.GetComponent<Animator>().SetBool("isDead", true);
-     //   }
-   // }
-
-    
-
+    }  
+}     
